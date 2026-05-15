fb50827c484513bb9fb9b3e8d142f6e70df53606
kiennguyenhoang610 <kiennguyenhoang610@gmail.com>
2026-05-15 22:22:26 +0700

    Remove redundant version

--- TreePredict.py
+++
@@ -1,8 +0,0 @@
-def PredictValue(stock_type="BTC", days=1):
-    from app import app
-    from services.model_service import ModelService
-
-    with app.app_context():
-        service = ModelService(app.config)
-        result = service.predict(stock_type, days=days)
-        return result["dt_series"], result["rf_series"]
